abc6ee708988384fa2822b21cdeb8055b0e66cc3
Paulina Dziobek <dziovek@gmail.com>
2020-04-25 21:18:38 +0200

    favicon added

---
+++ build/precache-manifest.0a9fa85e2980d1c94b8809bf9fc9fbb3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b72c73718d0c9ae8008e5743a2b8711",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ad24148d74f8fd55e32",
+    "url": "/static/css/main.5ca8964b.chunk.css"
+  },
+  {
+    "revision": "09d353c608711a731e2b",
+    "url": "/static/js/2.0d51bb75.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0d51bb75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ad24148d74f8fd55e32",
+    "url": "/static/js/main.0bf8d9bb.chunk.js"
+  },
+  {
+    "revision": "2b2ab0a2dc946ac45972",
+    "url": "/static/js/runtime-main.1ae3753d.js"
+  },
+  {
+    "revision": "9bb07460fa7ff519d831ba4ba1a2ecb5",
+    "url": "/static/media/insta-icon.9bb07460.svg"
+  }
+]);
